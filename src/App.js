742985614f0a66fded60fb72d294ae85cae096cd
yamin <yamin2008@live.com>
2021-06-29 12:53:03 +0100

    novo Tutorial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class App extends React.Component {
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello, ppl</h1>
+            </div>
+        )
+    }
+}
+
